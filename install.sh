0fc23b23376ffb6dd491d82c9eab816ffacbdbdf
Ferreol <ferreolgodebarge@gmail.com>
2019-01-03 13:26:55 +0100

    adding bash script to deploy monitoring stack

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Remove old directories if exists
+rm -rf prometheus*
+rm -rf grafana*
+
+# Kill processes
+pkill prometheus
+pkill grafana
+pkill python
+
+pkill prometheus
+# Install Prometheus binaries
+wget https://github.com/prometheus/prometheus/releases/download/v2.6.0/prometheus-2.6.0.linux-amd64.tar.gz -O prometheus.tar.gz
+tar -xvf prometheus.tar.gz
+rm prometheus.tar.gz
+
+# Install Grafana binaries
+wget https://dl.grafana.com/oss/release/grafana-5.4.2.linux-amd64.tar.gz -O grafana.tar.gz
+tar -xvf grafana.tar.gz
+rm grafana.tar.gz
+
+PROM_HOME=prometheus-2.6.0.linux-amd64
+GRAF_HOME=grafana-5.4.2
+
+# Copy prometheus conf
+cp conf/prometheus.yml $PROM_HOME/prometheus.yml
+cp conf/defaults.ini $GRAF_HOME/conf/defaults.ini
+
+
+# Run prometheus client
+python client.py &>/dev/null &
+
+# Run prometheus
+./$PROM_HOME/prometheus --web.listen-address="0.0.0.0:9090" --config.file=$PROM_HOME/prometheus.yml --storage.tsdb.path=$PROM_HOME/data/ &>/dev/null &
+
+# Run grafana
+./$GRAF_HOME/bin/grafana-server -homepath=$GRAF_HOME/ &>/dev/null &
